a5382859dd3b3443ac54d56744455fa762acc9ee
Leguizamon Marcos <marcos.leguizamon@kapsch.net>
2026-07-31 18:12:42 -0300

    lockup white?

--- data/social-data.js
+++ data/social-data.js
@@ -3,21 +3,24 @@
    =========================================================
    Cómo editar:
    1. Cada red es un bloque { ... }.
-   2. icon: emoji o símbolo que se muestra.
-   3. name: nombre de la red.
-   4. handle: tu usuario/nombre visible ahí.
-   5. link: URL completa a tu perfil.
-   6. Para agregar una red nueva, copiá un bloque y modificalo.
+   2. icon: clase de ícono de Font Awesome (ya son íconos reales
+      de cada marca, no emojis). No hace falta tocar esto salvo
+      que quieras cambiar el ícono.
+   3. color: color de marca que se usa en el ícono.
+   4. name: nombre de la red.
+   5. handle: tu usuario/nombre visible ahí.
+   6. link: URL completa a tu perfil.
+   7. Para agregar una red nueva, copiá un bloque y modificalo.
       Para sacar una red, borrá su bloque completo (con la coma).
    ========================================================= */
 
 const socialData = [
-  {icon:'▶️', name:'YouTube',   handle:'@viajandoalmundo',      link:'https://www.youtube.com/@viajandoalmundo'},
-  {icon:'📷', name:'Instagram', handle:'@viajando.almundo',     link:'https://www.instagram.com/viajando.almundo'},
-  {icon:'🎵', name:'TikTok',    handle:'@viajando.almundo',     link:'https://www.tiktok.com/@viajando.almundo'},
-  {icon:'f',  name:'Facebook',  handle:'viajandoalmundo.ok',    link:'https://www.facebook.com/viajandoalmundo.ok'},
-  {icon:'✕',  name:'X',         handle:'@_viajando_mundo',      link:'https://x.com/_viajando_mundo'},
-  {icon:'@',  name:'Threads',   handle:'@viajando.almundo',     link:'https://www.threads.com/@viajando.almundo'},
-  {icon:'📌', name:'Pinterest', handle:'viajandoam',            link:'https://ar.pinterest.com/viajandoam/'},
-  {icon:'🛍️', name:'Gumroad',   handle:'Guías digitales',       link:'https://gumroad.com/viajandoalmundo'},
+  {icon:'fa-brands fa-youtube',    color:'#FF0000', name:'YouTube',   handle:'@viajandoalmundo',   link:'https://www.youtube.com/@viajandoalmundo'},
+  {icon:'fa-brands fa-instagram',  color:'#E1306C', name:'Instagram', handle:'@viajando.almundo',  link:'https://www.instagram.com/viajando.almundo'},
+  {icon:'fa-brands fa-tiktok',     color:'#000000', name:'TikTok',    handle:'@viajando.almundo',  link:'https://www.tiktok.com/@viajando.almundo'},
+  {icon:'fa-brands fa-facebook',   color:'#1877F2', name:'Facebook',  handle:'viajandoalmundo.ok', link:'https://www.facebook.com/viajandoalmundo.ok'},
+  {icon:'fa-brands fa-x-twitter',  color:'#000000', name:'X',         handle:'@_viajando_mundo',   link:'https://x.com/_viajando_mundo'},
+  {icon:'fa-brands fa-threads',    color:'#000000', name:'Threads',   handle:'@viajando.almundo',  link:'https://www.threads.com/@viajando.almundo'},
+  {icon:'fa-brands fa-pinterest',  color:'#E60023', name:'Pinterest', handle:'viajandoam',         link:'https://ar.pinterest.com/viajandoam/'},
+  {icon:'fa-solid fa-bag-shopping',color:'#FF90E8', name:'Gumroad',   handle:'Guías digitales',    link:'https://gumroad.com/viajandoalmundo'},
 ];
